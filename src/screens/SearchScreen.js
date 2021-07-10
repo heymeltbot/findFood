b251b50e7d43baebdd65a488ee3db4189663492d
Lana Hayes Redmond <lana.hayes@aspirant.com>
2021-07-10 17:19:16 -0400

    Add search bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar'
 
 const SearchScreen = () => {
+    const [term, setTerm] = useState('')
     return(
         <View>
+            <SearchBar 
+            term={term} 
+            onTermChange={(newTerm) => setTerm(newTerm)} 
+            onTermSubmit={console.log('i am the computer talking to you')}
+            />
         <Text>Hello Clarice</Text>
     </View>
     )
