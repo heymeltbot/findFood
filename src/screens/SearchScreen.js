4ce1651644ca05ee1ec72afff19758e1c5e7b881
Lana Hayes Redmond <lana.hayes@aspirant.com>
2021-07-10 14:33:57 -0400

    Initial installs and boilerplate

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+    return(
+        <View>
+        <Text>Hello Clarice</Text>
+    </View>
+    )
+    
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
