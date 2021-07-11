e1bc6b5149d73e0a5b7339f0f3b21e80a903b4e6
Lana Hayes Redmond <lana.hayes@aspirant.com>
2021-07-11 17:16:35 -0400

    Update app.js

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-import {createStackNavigator} from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation';
-import SearchScreen from './src/screens/SearchScreen';
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import SearchScreen from "./src/screens/SearchScreen";
+import ShowResultsScreen from "./src/screens/ShowResultsScreen";
 
-const navigator = createStackNavigator({
-  Search: SearchScreen
-
-},{
-  initialRouteName: 'Search',
-  defaultNavigationOptions: {
-    title: 'Find Food!'
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
+    ShowResults: ShowResultsScreen,
+  },
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Find Food!",
+    },
   }
-});
+);
 
 export default createAppContainer(navigator);
